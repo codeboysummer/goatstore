523ff93cecca5c4a6f4f304dc9b428ff16cc5153
codeboysummer <aw.wahid13@gmail.com>
2023-01-31 15:02:00 -0500

    'created redux slices '

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -18,14 +18,47 @@ const usernameSlice = createSlice({
     },
   },
 });
+const emailInputSlice = createSlice({
+  name: "email",
+  initialState: { value: '' },
+  reducers: {
+    setemail: (state, action) => {
+      state.value = action.payload;
+    },
+  },
+});
+
+const passwordInputSlice = createSlice({
+  name: "password",
+  initialState: { value: '' },
+  reducers: {
+    setpassword: (state, action) => {
+      state.value = action.payload;
+    },
+  },
+});
+
+const userNameValidity = createSlice({
+  name: "usernameIsValid",
+  initialState: { value: true },
+  reducers: {
+    setusernameIsValid: (state, action) => {
+      return { ...state, value: action.payload };
+    },
+  },
+});
 
 export const { setUser } = userSlice.actions;
+export const { setusernameIsValid } = userNameValidity.actions;
+export const { setpassword } = passwordInputSlice.actions;
+export const { setemail } = emailInputSlice.actions;
 
 export const { setUsername } = usernameSlice.actions;
 
 export default {
-  counter: counterSlice.reducer,
-  word: wordSlice.reducer,
   user: userSlice.reducer,
+  usernameIsValid: userNameValidity.reducer,
   username: usernameSlice.reducer,
+  email: emailInputSlice.reducer,
+  password: passwordInputSlice.reducer,
 };
