e6b594d4c7a75bbaa911dec149a53f119a66225e
codeboysummer <aw.wahid13@gmail.com>
2023-01-31 15:02:45 -0500

    'registration for component created'

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
-import { ChakraProvider } from '@chakra-ui/react'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { ChakraProvider } from "@chakra-ui/react";
+import { Provider } from "react-redux";
+import store from "../src/redux/configStore";
 
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <ChakraProvider>
-    <React.StrictMode>
-
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <ChakraProvider>
+      <React.StrictMode>
         <App />
-      
-    </React.StrictMode>
-  </ChakraProvider>
-)
+      </React.StrictMode>
+    </ChakraProvider>
+  </Provider>
+);
