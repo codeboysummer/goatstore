8efb780f612999626739ae54a1a6c3fa8f78db67
codeboysummer <aw.wahid13@gmail.com>
2023-02-15 08:25:22 -0800

    'creating input to add list title'

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,9 +1,12 @@
-import React from "react";
+import { AddIcon, CheckIcon } from "@chakra-ui/icons";
+import { Box, Button, Flex, IconButton, Input, InputGroup, InputRightElement, useDisclosure } from "@chakra-ui/react";
+import React, { useState } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { useNavigate } from "react-router-dom";
 import Layout from "../components/Layout";
 import TrelloCard from "../components/TrelloCard";
 import { auth } from "../firebase/firebase";
+import { useOutsideClick } from '@chakra-ui/react'
 
 const Dashboard = () => {
   const authstate = useAuthState(auth);
@@ -31,9 +34,53 @@ const Dashboard = () => {
   return (
     <Layout>
       
+      <Flex w={'100%'}  >
       <TrelloCard data={sampleData}/>
+
+    <CreateList>
+        <IconButton 
+        
+        pos={'fixed'} top={'11%'} right={'1%'} colorScheme={'twitter'} icon={<AddIcon />}/>
+      
+      
+    </CreateList>
+      </Flex>
     </Layout>
   );
 };
 
 export default Dashboard;
+
+function CreateList({children}){
+const [showInput, setshowInput] = useState(false)
+const ref = React.useRef()
+
+useOutsideClick({
+  ref: ref,
+  handler: () => setshowInput(false),
+})
+
+  return <>
+
+  <Box ref={ref}  onClick={()=>setshowInput(true)} >
+    {showInput?<Box  pos={'fixed'} top={'11%'} right={'1%'}  ref={ref}>
+    <InputGroup size='md'>
+      <Input
+        pr='5rem'
+        placeholder='title'
+      />
+      <InputRightElement width='4.5rem'>
+       <IconButton icon={<CheckIcon
+       />}/>
+      </InputRightElement>
+    </InputGroup>
+    </Box>:children}
+    
+  </Box>
+  
+  </>
+
+}
+
+
+
